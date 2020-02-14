8480dd2b5dc18bf3c0800c3a12cc5c6d24fb5eac
David James <davidjelst@gmail.com>
2020-02-14 10:12:50 -0600

    added secrets.py file

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .secrets import my_secret_key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k1@3_buxc9a_z@c()&@v)84n9sig6kx$q1%dn^(bx6yzlpl+q)'
+SECRET_KEY = my_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
